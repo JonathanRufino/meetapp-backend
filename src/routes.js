fbcf29cffe80bb0fd0f98aded1dc3963820de763
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-06-29 11:51:26 -0300

    chore: Add code style config

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Meetaap'});
+  res.json({ message: 'Hello Meetaap' });
 });
 
 export default routes;
