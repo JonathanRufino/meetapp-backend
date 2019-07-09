0d28d6a12da8fe30e939ad91e223ddd83716410f
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-08 21:32:15 -0300

    feat: Update meetup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -34,6 +34,45 @@ class MeetupController {
 
     return res.json(meetup);
   }
+
+  async update(req, res) {
+    const schema = yup.object().shape({
+      title: yup.string().required(),
+      description: yup.string().required(),
+      location: yup.string().required(),
+      date: yup.date().required(),
+      banner_id: yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: "You don't have permission to edit this meetup." });
+    }
+
+    const { title, description, location, date, banner_id } = req.body;
+
+    if (isBefore(parse(date), new Date())) {
+      return res.status(401).json({ error: 'Past dates are not allowed.' });
+    }
+
+    const meetupUpdated = await Meetup.create({
+      title,
+      description,
+      location,
+      date,
+      banner_id,
+      user_id: req.userId,
+    });
+
+    return res.json(meetupUpdated);
+  }
 }
 
 export default new MeetupController();
