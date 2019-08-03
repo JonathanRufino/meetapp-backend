cf9d3d1f97c74a6246d93ceba981a4793212460b
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-08-02 22:36:56 -0300

    refactor: return banner data in organizing list

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,9 +1,18 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class OrganizingController {
   async index(req, res) {
     const userMeetups = await Meetup.findAll({
       where: { user_id: req.userId },
+      attributes: ['id', 'title', 'description', 'location', 'date'],
+      include: [
+        {
+          model: File,
+          as: 'banner',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
     });
 
     return res.json(userMeetups);
