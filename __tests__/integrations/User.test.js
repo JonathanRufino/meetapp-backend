e7c1f2d670892b7896f89aae1f92043f2a8fa4ec
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-08-18 09:40:25 -0300

    test(user): encrypt password on user creation

--- __tests__/integrations/User.test.js
+++ __tests__/integrations/User.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 
 import app from '../../src/app';
 
@@ -46,5 +47,15 @@ describe('User', () => {
 
     expect(response.status).toBe(400);
     expect(response.body.error).toBe('User already exists.');
+  });
+
+  it('should encrypt password when new user is created', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 });
